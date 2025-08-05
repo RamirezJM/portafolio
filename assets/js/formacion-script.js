aca0142f49e13bdf8be0881134c66d05bdfba562
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-08-04 23:26:41 -0400

    formation sort

--- assets/js/formacion-script.js
+++ assets/js/formacion-script.js
@@ -18,18 +18,39 @@ async function dataLoad(tabId, jsonData) {
   try {
     const res = await fetch(jsonData)
     const data = await res.json()
-  /*   console.log(data) */
-    if (data[0].id !== undefined) {
+    /*   console.log(data) */
+  /*   if (data[0].estudiando) {
+      data.sort((a, b) => a.estudiando - b.estudiando)
+    } else if (data[0].id !== undefined) {
       data.sort((a, b) => a.id - b.id)
     } else {
       data.sort((a, b) => b.horas - a.horas)
-    }
+    } */
+   data.sort((a, b) => {
+  // Primero: ordenar por 'estudiando' (true primero)
+  if (a.estudiando !== b.estudiando) {
+    return (b.estudiando ? 1 : 0) - (a.estudiando ? 1 : 0);
+  }
+
+  // Si ambos están estudiando o no, pasamos al criterio secundario:
+  if (a.id !== undefined && b.id !== undefined) {
+    return a.id - b.id; // Para formación complementaria
+  } else {
+    return b.horas - a.horas; // Para formación técnica
+  }
+});
 
     data.forEach(item => {
       const card = document.createElement('div')
       card.className = 'cert-card'
       let content
-      if (item.horas == null && item.link == null)
+      if (item.estudiando) {
+        content = `
+                    <a href="${item.link}" target="_blank" rel="noopener noreferrer" class="current"><h3>${item.titulo}</a></h3>
+                    <p>${item.institucion}</p>
+                    <p>(estudiando)</p>
+                `
+      } else if (item.horas == null && item.link == null)
         content = `
                     <h3>${item.titulo}</h3>
                     <p>${item.institucion}</p>
